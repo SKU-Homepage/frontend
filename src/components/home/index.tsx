6d8d317c00523bbd8bc4a8298e0db6d2c5520561
YeChan Kim <az20058@naver.com>
2025-02-06 17:55:31 +0900

    🎨 Design : 스케일 조정 가로 500 -> 가로 393px

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,10 +1,10 @@
+import { ChildrenProp } from "@/utils/children.type";
 import Image from "next/image";
 import Link from "next/link";
-import { ReactNode } from "react";
 
 export const Home = ({ children }: ChildrenProp) => {
   return (
-    <div className="flex flex-col bg-[#F6F6F6] bg-[url('/images/home_background.png')] bg-no-repeat items-center px-[24px] h-[calc(100vh-90px)] overflow-y-auto">
+    <div className="flex box flex-col bg-[#F6F6F6] bg-[url('/images/home_background.png')] bg-no-repeat items-center px-[24px] h-[calc(100vh-75px)] overflow-y-auto">
       {children}
     </div>
   );
@@ -12,9 +12,9 @@ export const Home = ({ children }: ChildrenProp) => {
 
 const Header = () => {
   return (
-    <header className="flex flex-col leading-normal text-[#fff] w-full pt-[46px] pb-[22px] gap-[3px]">
-      <span className="text-[24px] font-bold">안녕하세요! 서경대님!</span>
-      <span className="text-[24px] font-medium">오늘도 열심히 준비해볼까요?</span>
+    <header className="flex flex-col leading-normal text-[#fff] w-full pt-[36px] pb-[21px] gap-[6px]">
+      <span className="text-[18.86px] font-bold">안녕하세요! 서경대님!</span>
+      <span className="text-[18.86px] font-medium">오늘도 열심히 준비해볼까요?</span>
     </header>
   );
 };
@@ -27,23 +27,26 @@ const TimeTable = ({ children }: ChildrenProp) => {
   }요일`;
 
   return (
-    <div className="flex w-full p-[24px] bg-[#FFF] flex-col border-[0.5px] border-[rgba(228, 228, 228, 1)] rounded-[15px] shadow-[0_4px_8.2px_0px_rgba(77,102,236,0.08)]">
-      <span className="text-[12px] leading-normal text-[#75869B] ml-[40px]">{today}</span>
-      <div className="flex items-center gap-[7px] text-[20px]">
-        <Image src="/images/sound-speaker.svg" width={30} height={30} alt="사운드 스피커" />
+    <div className="flex w-full p-[18px] bg-[#FFF] flex-col border-[0.5px] border-[rgba(228, 228, 228, 1)] rounded-[15px] shadow-[0_4px_8.2px_0px_rgba(77,102,236,0.08)]">
+      <div className="flex items-center gap-[7px]">
+        <div className="w-[23px]" />
+        <span className="text-[9px] leading-normal text-[#75869B]">{today}</span>
+      </div>
+      <div className="flex items-center gap-[7px] text-[15.7px]">
+        <Image src="/images/sound-speaker.svg" width={23} height={23} alt="사운드 스피커" />
         <div className="leading-normal text-[#143967] font-semibold">
           <span>오늘은 </span>
           <span className="text-[#0072FF]">2개의</span>
           <span> 수업이 있어요</span>
         </div>
       </div>
-      <div className="flex flex-col gap-[10px] mt-[21px]">{children}</div>
+      <div className="flex flex-col gap-[7.8px] mt-[16.5px]">{children}</div>
       <Link
         href="#"
-        className="flex justify-end items-center gap-[3px] w-full text-end text-[12px] mt-[11px] opacity-56 text-[#75869B]"
+        className="flex justify-end items-center gap-[3px] w-full text-end text-[9px] mt-[9px] opacity-56 text-[#75869B]"
       >
         <span>내 시간표 확인하기</span>
-        <Image src="/images/direction.svg" alt="화살표" width={7} height={11} />
+        <Image src="/images/direction.svg" alt="화살표" width={5} height={8} />
       </Link>
     </div>
   );
@@ -51,11 +54,11 @@ const TimeTable = ({ children }: ChildrenProp) => {
 
 const Lecture = () => {
   return (
-    <div className="flex flex-col gap-[11px] w-full rounded-[10px] px-[22px] py-[15px] font-medium justify-center text-[#143967] bg-linear-[247deg,rgba(222,234,255,0.08)_-8.71%,rgba(154,191,255,0.15)_108.48%,rgba(200,217,237,0.16)]">
-      <p className="text-[14px] ">10:00 - 11:50</p>
+    <div className="flex flex-col gap-[11px] w-full rounded-[10px] px-[16px] py-[11px] font-medium justify-center text-[#143967] bg-linear-[247deg,rgba(222,234,255,0.08)_-8.71%,rgba(154,191,255,0.15)_108.48%,rgba(200,217,237,0.16)]">
+      <p className="text-[11px] ">10:00 - 11:50</p>
       <div className="flex justify-between items-end w-full">
-        <span className="text-[20px] font-semibold">World English2</span>
-        <span className="text-[14px]">은주관 505</span>
+        <span className="text-[15px] font-semibold">World English2</span>
+        <span className="text-[11px]">은주관 505</span>
       </div>
     </div>
   );
@@ -63,9 +66,9 @@ const Lecture = () => {
 
 const Tip = () => {
   return (
-    <div className="flex gap-[14.5px] w-full h-[48px] px-[22px] py-[14px] mt-[28px] mb-[18px] rounded-[12px] bg-[#E9EFF7] items-center">
-      <span className="text-[16px] font-bold">TIP !</span>
-      <span className="text-[14px]">위젯을 자유롭게 설정해봐요</span>
+    <div className="flex gap-[14.5px] w-full h-[48px] px-[17px] py-[12px] mt-[22px] mb-[14px] rounded-[12px] bg-[#E9EFF7] items-center">
+      <span className="text-[12px] font-bold">TIP !</span>
+      <span className="text-[11px]">위젯을 자유롭게 설정해봐요</span>
     </div>
   );
 };
@@ -76,15 +79,17 @@ const WidgetWrapper = ({ children }: ChildrenProp) => {
 
 const Widget = () => {
   return (
-    <div className="flex flex-col p-[22px] !bg-[white] rounded-[15px]">
-      <Link className="aspect-square" href="#">
-        <div className="flex w-full justify-between mb-[6px] text-[20px] font-semibold items-center text-[#143967]">
-          <h2>공지사항</h2>
-          <Image src="/images/direction.svg" alt="화살표" width={11} height={18} />
+    <div className="flex flex-col p-[15px] !bg-[white] rounded-[15px]">
+      <Link className="aspect-square flex flex-col justify-between" href="#">
+        <div>
+          <div className="flex w-full justify-between mb-[4.7px] text-[15.7px] font-semibold items-center text-[#143967]">
+            <h2>공지사항</h2>
+            <Image src="/images/direction.svg" alt="화살표" width={8.6} height={14} />
+          </div>
+          <p className="text-[12px] font-medium">공지사항을 빠르게 확인해보세요.</p>
         </div>
-        <p className="text-[12px] font-medium mb-[10px]">공지사항을 빠르게 확인해보세요.</p>
         <div className="flex w-full justify-end">
-          <Image src="/images/color-speaker.svg" width={45} height={45} alt="공지사항" />
+          <Image src="/images/color-speaker.svg" width={58} height={58} alt="공지사항" />
         </div>
       </Link>
     </div>
@@ -98,10 +103,6 @@ const AddWidget = () => {
     </button>
   );
 };
-
-interface ChildrenProp {
-  children: ReactNode;
-}
 
 Home.Header = Header;
 Home.TimeTable = TimeTable;
--- src/components/tabBar/Containers.tsx
+++ src/components/tabBar/Containers.tsx
@@ -1,12 +1,13 @@
 "use cient";
 
+import { ChildrenProp } from "@/utils/children.type";
 import Image from "next/image";
 import Link from "next/link";
-import { ReactNode, useState } from "react";
+import { useState } from "react";
 
 export const Containers = ({ children }: ChildrenProp) => {
   return (
-    <div className="flex z-10 justify-between pt-[10px] items-start h-[95px] px-[51px] fixed bottom-0 w-full shadow-[4px_0px_7px_0px_rgba(0,0,0,0.04)]">
+    <div className="flex z-10 bg-[#fff] justify-between pt-[10px] items-start h-[75px] px-[51px] fixed bottom-0 w-full shadow-[4px_0px_7px_0px_rgba(0,0,0,0.04)]">
       {children}
     </div>
   );
@@ -31,12 +32,12 @@ const Item = ({ src, alt, href, label, isActive }: ItemProps) => {
     >
       <Image
         src={src}
-        width={24}
-        height={24}
+        width={18}
+        height={18}
         alt={alt}
         className={`transition duration-200 ease-in-out ${isTouched ? "scale-140" : ""}`}
       />
-      <span className={`text-[13px] ${isActive ? "text-[#4F68E9]" : "text-[#BDBDBD]"}`}>
+      <span className={`text-[10px] ${isActive ? "text-[#4F68E9]" : "text-[#BDBDBD]"}`}>
         {label}
       </span>
     </Link>
@@ -49,10 +50,6 @@ interface ItemProps {
   href: string;
   label: string;
   isActive: boolean;
-}
-
-interface ChildrenProp {
-  children: ReactNode;
 }
 
 Containers.Item = Item;
