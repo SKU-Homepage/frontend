586529747b3a83a9ae0d757ec8d65eb4c942bbc8
YeChan Kim <az20058@naver.com>
2025-02-06 17:53:09 +0900

    🎨 Design : 로그인 UI 제작

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import L from "@/components/login";
+import LoginBtn from "@/components/login/LoginBtn";
+
+export default function Login() {
+  return (
+    <L>
+      <L.Content>
+        <LoginBtn />
+      </L.Content>
+    </L>
+  );
+}
