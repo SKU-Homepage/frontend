ee7f812a1c6396b4d794a5ad5ea17224e1bc7494
changhoyu <tyu9817@naver.com>
2025-02-08 02:16:27 +0900

    ✨Feat: create page.tsx/ create layout.tsx/ add max-width and margin, layout.tsx

---
+++ src/app/notice/layout.tsx
@@ -0,0 +1,8 @@
+export default function NoticeLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="w-full max-w-[600px] mx-auto">
+      <p>NoticeLayout</p>
+      {children}
+    </div>
+  );
+}
---
+++ src/app/notice/page.tsx
@@ -0,0 +1,3 @@
+export default function Notice() {
+  return <p>Notice Page</p>;
+}
