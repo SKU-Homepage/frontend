43cf06b68b0aef7b8b72df507081c8956add8920
YeChan Kim <az20058@naver.com>
2025-02-08 23:31:10 +0900

    ♻️ Refactor : Tip 컴포넌트 common으로 이동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Tip from "@/components/common/Tip";
 import H from "@/components/home";
 import Table from "@/components/home/TableSection";
 import { WidgetSection } from "@/components/home/WidgetSection";
@@ -7,7 +8,7 @@ export default function Home() {
     <H>
       <H.Header />
       <Table />
-      <H.Tip />
+      <Tip msg="위젯을 자유롭게 설정해보세요." />
       <WidgetSection />
     </H>
   );
