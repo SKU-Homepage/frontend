338c1231aaff9f1433153a8da3d474b26e3b4c48
changhoyu <tyu9817@naver.com>
2025-02-10 01:03:32 +0900

    Refactor: add ReactNode and update code from React.ReactNdoe to ReacNode, NoticeLayout.tsx

--- src/app/notice/layout.tsx
+++ src/app/notice/layout.tsx
@@ -1,4 +1,6 @@
-export default function NoticeLayout({ children }: { children: React.ReactNode }) {
+import { ReactNode } from "react";
+
+export default function NoticeLayout({ children }: { children: ReactNode }) {
   return (
     <div className="w-full max-w-[600px] min-h-screen h-full mx-auto px-[4.6%] bg-[#F6F6F6]">
       {children}
