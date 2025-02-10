8248caa4d3fe3ac422e35e77fbb0508a7c8ec571
changhoyu <tyu9817@naver.com>
2025-02-10 16:32:02 +0900

    ✨Feat: create NoticeBtn UI, NoticeBtnSetcion

--- src/app/notice/page.tsx
+++ src/app/notice/page.tsx
@@ -1,4 +1,4 @@
-import { Header, TimeInformation } from "@/components/notice";
+import { Header, NoticeBtn, NoticeBtnSection, TimeInformation } from "@/components/notice";
 
 export default function Notice() {
   return (
@@ -8,7 +8,13 @@ export default function Notice() {
         mention="전체 학사 일정과 개인 일정을 추가하여 한눈에 정리할 수 있어요"
       />
       <TimeInformation />
-      <p>Notice Page</p>
+      <NoticeBtnSection>
+        <NoticeBtn
+          type="normal"
+          department="학생처 2024.12.02"
+          noticeTitle="2024 콘텐츠 인사이트 : 상상은 현실이 된다"
+        />
+      </NoticeBtnSection>
     </>
   );
 }
