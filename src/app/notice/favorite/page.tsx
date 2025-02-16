22e22720a3d16873df424a2baff8324f445928cd
changhoyu <tyu9817@naver.com>
2025-02-17 00:38:05 +0900

    ✨Feat: add Header within FavoriteNotificationPage

---
+++ src/app/notice/favorite/page.tsx
@@ -0,0 +1,12 @@
+import { Header } from "@/components/notice";
+
+export default function FavoriteNotificationPage() {
+  return (
+    <>
+      <Header
+        title="찜 목록"
+        mention="전체 학사 일정과 개인 일정을 추가하여 한눈에 정리할 수 있어요"
+      />
+    </>
+  );
+}
