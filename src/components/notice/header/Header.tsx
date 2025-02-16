8037901ccc454f58446cf287eb56f6594fce8f39
changhoyu <tyu9817@naver.com>
2025-02-16 14:09:09 +0900

    ♻️Refactor: separte Header into HeaderWithNav, Header

--- src/components/notice/header/Header.tsx
+++ src/components/notice/header/Header.tsx
@@ -1,8 +1,3 @@
-"use client";
-
-import { useState } from "react";
-import HeaderNavItem from "./Header.NavItem";
-import HeaderNavLayout from "./Header.NavLayout";
 import HeaderRootLayout from "./Header.RootLayout";
 import HeaderTitle from "./Header.Title";
 
@@ -12,38 +7,9 @@ interface HeaderProps {
 }
 
 const Header = ({ title, mention }: HeaderProps) => {
-  const [isClicked, setIsCliked] = useState({
-    allNoctice: true,
-    calledDibsOnNotice: false,
-  });
   return (
     <HeaderRootLayout>
       <HeaderTitle title={title} mention={mention} />
-
-      <HeaderNavLayout>
-        <HeaderNavItem
-          pathName="전체"
-          isClicked={isClicked.allNoctice}
-          onClick={() => {
-            setIsCliked((prev) => ({
-              ...prev,
-              allNoctice: true,
-              calledDibsOnNotice: false,
-            }));
-          }}
-        />
-        <HeaderNavItem
-          pathName="키워드"
-          isClicked={isClicked.calledDibsOnNotice}
-          onClick={() => {
-            setIsCliked((prev) => ({
-              ...prev,
-              allNoctice: false,
-              calledDibsOnNotice: true,
-            }));
-          }}
-        />
-      </HeaderNavLayout>
     </HeaderRootLayout>
   );
 };
