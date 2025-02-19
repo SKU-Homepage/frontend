1948012846fac0f70305ad4c6737d64c0e88352a
YeChan Kim <az20058@naver.com>
2025-02-20 00:56:56 +0900

    🎨 Design : 마이페이지 헤더 컴포넌트 추가 및 세부 디자인 변경

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,12 @@
 import P from "@/components/profile";
 import Service from "../../components/profile/Service";
-import InfoSection from "@/components/profile/InfoSection";
+import InfoSection from "@/components/profile/InfoSection/InfoSection";
+import Header from "@/components/header/Header";
 
 export default function Profile() {
   return (
     <P>
-      <P.Header />
+      <Header title="마이페이지" />
       <InfoSection />
       <P.WidgetSection />
       <Service />
