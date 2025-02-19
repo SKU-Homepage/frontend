63a2c4af61317a7e1ffc2c66605a76d25e43e932
YeChan Kim <az20058@naver.com>
2025-02-20 01:23:21 +0900

    🐛 Fix : header 컴포넌트의 title props도 필수->선택으로 변경

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,7 +5,7 @@ export default function Header({ src, msg, title, onClick }: HeaderProps) {
   return (
     <header className="fixed top-0 z-20 flex h-[73px] w-full items-center justify-between bg-white px-[18px]">
       <BackBtn />
-      <h1 className="text-[17px] font-semibold">{title}</h1>
+      <h1 className="w-[40%] text-center text-[17px] font-semibold">{title}</h1>
       <HeaderBtn src={src} msg={msg} onClick={onClick} />
     </header>
   );
@@ -14,6 +14,6 @@ export default function Header({ src, msg, title, onClick }: HeaderProps) {
 interface HeaderProps {
   src?: string;
   msg?: string;
-  title: string;
+  title?: string;
   onClick?: (boolean: boolean) => void;
 }
