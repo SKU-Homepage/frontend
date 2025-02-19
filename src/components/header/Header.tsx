c925fc0363e5100943f89b7d332c34dce8e9cccf
YeChan Kim <az20058@naver.com>
2025-02-20 01:16:42 +0900

    🎨 Design : z-index 설정

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,7 +3,7 @@ import BackBtn from "./BackBtn";
 
 export default function Header({ src, msg, title, onClick }: HeaderProps) {
   return (
-    <header className="flex justify-between items-center bg-white fixed top-0 px-[18px] h-[73px] w-full">
+    <header className="fixed top-0 z-20 flex h-[73px] w-full items-center justify-between bg-white px-[18px]">
       <BackBtn />
       <h1 className="text-[17px] font-semibold">{title}</h1>
       <HeaderBtn src={src} msg={msg} onClick={onClick} />
