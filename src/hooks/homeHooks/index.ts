34d7d4df75b986991abdaceaa31de073114cd8b0
YeChan Kim <az20058@naver.com>
2025-02-26 04:05:57 +0900

    ✨ API : 메인페이지 api 연동

---
+++ src/hooks/homeHooks/index.ts
@@ -0,0 +1,27 @@
+import { privateApi } from "@/api/axios";
+import { queryOptions } from "@tanstack/react-query";
+
+export const getTodayTimeTable = async () => {
+  try {
+    const baseURL = "/time-table/today";
+    const res = await privateApi.get(baseURL);
+    const data = res.data;
+    return data.result.timeTables;
+  } catch {
+    return null;
+  }
+};
+
+export const useTimeTable = queryOptions({
+  queryKey: ["time-table"],
+  queryFn: getTodayTimeTable,
+  staleTime: 1000 * 60 * 60,
+  enabled: false,
+});
+
+export type TimeTable = {
+  time?: string;
+  subject: string;
+  classroom: string;
+  classtime: string;
+};
