1156c288123725eee786b63099cf97a1b8c0c482
YeChan Kim <az20058@naver.com>
2025-02-27 03:27:07 +0900

    ✨ Feat : 오늘의 시간표 목록 없을 시 예외 처리 UI 추가

--- src/components/home/TimeTable/Lecture.tsx
+++ src/components/home/TimeTable/Lecture.tsx
@@ -1,4 +1,6 @@
 import { TimeTable } from "@/hooks/homeHooks";
+import { ChildrenProp } from "@/utils/children.type";
+import Image from "next/image";
 
 const Lecture = ({ subject, classroom, classtime }: TimeTable) => {
   return (
@@ -8,6 +10,22 @@ const Lecture = ({ subject, classroom, classtime }: TimeTable) => {
         <span className="text-[15px] font-semibold">{subject}</span>
         <span className="text-[11px]">{classroom}</span>
       </div>
+    </div>
+  );
+};
+
+export const LecturesContainer = ({ children }: ChildrenProp) => {
+  return <div className="mt-[16.5px] flex flex-col gap-[7.8px]">{children}</div>;
+};
+
+export const EmptyLecture = () => {
+  return (
+    <div className="flex w-full items-center justify-between rounded-[10px] bg-linear-[247deg,rgba(222,234,255,0.08)_-8.71%,rgba(154,191,255,0.15)_108.48%,rgba(200,217,237,0.16)] px-[19px] py-[14px]">
+      <div className="gap-[4px] text-[11px] font-medium text-[#75869B]">
+        <p>오늘은 예정된 수업이 없어요!</p>
+        <p>일정이나 수업이 있다면 추가해보세요</p>
+      </div>
+      <Image src="/images/cloud.png" width={47} height={37} alt="수업 없음" />
     </div>
   );
 };
--- src/components/home/TimeTable/Lectures.tsx
+++ src/components/home/TimeTable/Lectures.tsx
@@ -1,20 +1,24 @@
 "use client";
 
-import Lecture from "./Lecture";
+import Lecture, { EmptyLecture, LecturesContainer } from "./Lecture";
 import { TimeTable } from "@/hooks/homeHooks";
 
 const Lectures = ({ lectures }: LecturesProps) => {
   return (
-    <div className="mt-[16.5px] flex flex-col gap-[7.8px]">
-      {lectures.map((item: TimeTable) => (
-        <Lecture
-          key={item.subject}
-          subject={item.subject}
-          classroom={item.classroom}
-          classtime={item.classtime}
-        />
-      ))}
-    </div>
+    <LecturesContainer>
+      {lectures ? (
+        lectures.map((item: TimeTable) => (
+          <Lecture
+            key={item.subject}
+            subject={item.subject}
+            classroom={item.classroom}
+            classtime={item.classtime}
+          />
+        ))
+      ) : (
+        <EmptyLecture />
+      )}
+    </LecturesContainer>
   );
 };
 
