6b9524b7659a42828e7bf0e76bd93788ed4557af
changhoyu <tyu9817@naver.com>
2025-03-01 19:27:37 +0900

    🔥Remove : classNames within div tag

--- src/app/extracurricular/layout.tsx
+++ src/app/extracurricular/layout.tsx
@@ -1,31 +1,34 @@
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 
-import { HeaderWithBackButton, HeaderWithTitleAndMentionAndNav } from '@/components/common';
+import { HeaderWithBackButton, HeaderWithTitleAndMentionAndNav } from "@/components/common";
 
-
-export default function ExtraCurricularLayout({children}: {children: ReactNode}) {
-  return(
-    <div className='min-height: 100vh'>
+export default function ExtraCurricularLayout({ children }: { children: ReactNode }) {
+  return (
+    <>
       <HeaderWithBackButton />
-      <HeaderWithTitleAndMentionAndNav title='비교과 프로그램' mention='전체 학사 일정과 개인 일정을 추가하여 한 눈에 정리할 수 있어요' pathList={[
-        {
-          path: '/extracurricular',
-          pathName: '전체'
-        },
-        {
-          path: '/extracurricular/career-employment',
-          pathName: '진로취업'
-        },
-        {
-          path: '/extracurricular/education-career',
-          pathName: '교수학습'
-        },
-        {
-          path: '/extracurricular/uni-innovation',
-          pathName: '대학혁신'
-        },
-      ]}/>
+      <HeaderWithTitleAndMentionAndNav
+        title="비교과 프로그램"
+        mention="전체 학사 일정과 개인 일정을 추가하여 한 눈에 정리할 수 있어요"
+        pathList={[
+          {
+            path: "/extracurricular",
+            pathName: "전체",
+          },
+          {
+            path: "/extracurricular/career-employment",
+            pathName: "진로취업",
+          },
+          {
+            path: "/extracurricular/education-career",
+            pathName: "교수학습",
+          },
+          {
+            path: "/extracurricular/uni-innovation",
+            pathName: "대학혁신",
+          },
+        ]}
+      />
       {children}
-    </div>
+    </>
   );
 }
