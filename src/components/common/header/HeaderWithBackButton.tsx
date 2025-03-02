37f5dbdc9a4d2bc62200fb4db6dde886227b8fd1
changhoyu <tyu9817@naver.com>
2025-03-03 01:59:31 +0900

    ♻️Refactor : add alt to HeaderWithBackButton

--- src/components/common/header/HeaderWithBackButton.tsx
+++ src/components/common/header/HeaderWithBackButton.tsx
@@ -1,10 +1,11 @@
-import HeaderBackButton from './ui/Header.BackButton';
-import HeaderImageButton from './ui/Header.ImageButton';
+import HeaderBackButton from "./ui/Header.BackButton";
+import HeaderImageButton from "./ui/Header.ImageButton";
 
 interface HeaderWithBackButtonProps {
   src?: string;
   msg?: string;
   title?: string;
+  alt?: string;
   onClick?: (boolean: boolean) => void;
 }
 
@@ -12,16 +13,18 @@ interface HeaderWithBackButtonProps {
  * 최상단 뒤로 가기, 제목, 이미지 헤더
  * title(제목) : [optional]
  * src(이미지버튼 이미지) : [optional]
- * msg(글자, 이미지버튼 이미지 없을시) : [optional] 
-*/
-const HeaderWithBackButton = ({ src, msg, title, onClick }: HeaderWithBackButtonProps) => {
-  return(
-      <header className="fixed top-0 z-20 flex h-[73px] w-full max-w-[600px] items-center justify-between bg-white px-[18px]">
+ * alt(이미지버튼 alt) : [optional]
+ * msg(글자, 이미지버튼 이미지 없을시) : [optional]
+ */
+
+const HeaderWithBackButton = ({ src, msg, title, alt, onClick }: HeaderWithBackButtonProps) => {
+  return (
+    <header className="fixed top-0 z-20 flex h-[73px] w-full max-w-[600px] items-center justify-between bg-white px-[18px]">
       <HeaderBackButton />
       <h1 className="w-[40%] text-center text-[17px] font-semibold">{title}</h1>
-      <HeaderImageButton src={src} msg={msg} onClick={onClick} />
+      <HeaderImageButton src={src} msg={msg} alt={alt} onClick={onClick} />
     </header>
-  )
-}
+  );
+};
 
 export default HeaderWithBackButton;
