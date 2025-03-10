8673db9f69a2d02aa976ff9ba34630035e8ff409
changhoyu <tyu9817@naver.com>
2025-03-10 13:58:10 +0900

    🐛Fix : deploy issue

--- src/app/extracurricular/page.tsx
+++ src/app/extracurricular/page.tsx
@@ -4,10 +4,10 @@ import { getExtraCurricularPosts } from "@/api/extracurricular-service";
 import { getQueryClient } from "@/utils/get-query-client";
 import { ExtraCurricularPostSection } from "@/components/extraCurricular";
 
-export default function ExtraCurricularPage() {
+export default async function ExtraCurricularPage() {
   const queryClient = getQueryClient();
 
-  queryClient.prefetchQuery({
+  await queryClient.prefetchQuery({
     queryKey: ["extra-curricular-posts"],
     queryFn: getExtraCurricularPosts,
   });
