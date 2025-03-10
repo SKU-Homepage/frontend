b68d4792ed584e93e6da9f5f5b7f1cd07f0c6af3
changhoyu <tyu9817@naver.com>
2025-03-10 19:03:34 +0900

    ✨Feat : add ReactQueryStreamHydration

--- src/utils/Providers.tsx
+++ src/utils/Providers.tsx
@@ -2,9 +2,14 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import type * as React from "react";
 import { getQueryClient } from "./get-query-client";
+import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = getQueryClient();
 
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryStreamedHydration> {children}</ReactQueryStreamedHydration>
+    </QueryClientProvider>
+  );
 }
