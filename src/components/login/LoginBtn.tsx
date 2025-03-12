a6a746b7e57df1cfa8907761bb31629145fb4fd9
YeChan Kim <az20058@naver.com>
2025-03-13 00:04:23 +0900

    🐛 Fix : 리다이렉트 uri 환경변수 처리

--- src/components/login/LoginBtn.tsx
+++ src/components/login/LoginBtn.tsx
@@ -4,8 +4,7 @@ import Image from "next/image";
 
 const LoginBtn = () => {
   const handleLogin = () => {
-    location.href =
-      "https://accounts.google.com/o/oauth2/v2/auth?client_id=198007563456-703p8smpd6b7ujujqva20gn0qvjbm85a.apps.googleusercontent.com&redirect_uri=http://localhost:3000/api/auth/callback/google&response_type=code&scope=openid%20email%20profile";
+    location.href = `https://accounts.google.com/o/oauth2/v2/auth?client_id=198007563456-703p8smpd6b7ujujqva20gn0qvjbm85a.apps.googleusercontent.com&redirect_uri=${process.env.NEXT_PUBLIC_API_URL}/api/auth/callback/google&response_type=code&scope=openid%20email%20profile`;
   };
 
   return (
