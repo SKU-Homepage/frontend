a9a8bc599865777516c37fdeae5a17e5059d7478
changhoyu <tyu9817@naver.com>
2025-03-17 01:16:38 +0900

    ✨Feat : 무한 스크롤 구현현

--- src/components/notice/button/NoticeButtonSection.tsx
+++ src/components/notice/button/NoticeButtonSection.tsx
@@ -1,21 +1,46 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import NoticeBtn from "./NoticeBtn";
 import NoticeModal from "../modal/NoticeModal";
+import { useSuspenseInfiniteQuery } from "@tanstack/react-query";
+import { getNoticeByPageAndSearchKeyword } from "@/api/notice-service";
+import { useInView } from "react-intersection-observer";
 
 const NoticeButtonSection = () => {
   const [isOpen, setIsOpen] = useState(false);
+
+  const { ref, inView } = useInView();
+
+  const { data, fetchNextPage } = useSuspenseInfiniteQuery({
+    queryKey: ["notice"],
+    queryFn: ({ pageParam = 0 }) => getNoticeByPageAndSearchKeyword(pageParam),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, allPages) => (allPages.length <= 9 ? allPages.length : undefined),
+  });
+
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [inView, fetchNextPage]);
+
   return (
     <section>
-      <NoticeBtn
-        noticeTitle="2024 콘텐츠 인사이트 : 상상은 현실이 된다"
-        type="normal"
-        department="학생처 2024.12.02"
-        onClick={() => setIsOpen(true)}
-      />
+      {data.pages.map((page) =>
+        page.map((notice) => (
+          <NoticeBtn
+            key={notice.id}
+            noticeTitle={notice.title}
+            type="normal"
+            department={notice.author}
+            onClick={() => setIsOpen(true)}
+          />
+        ))
+      )}
       <NoticeModal isOpen={isOpen} onClose={setIsOpen} />
+      <div ref={ref}></div>
     </section>
   );
 };
