576129ccd97dfd46f4f55b3070ea75991a53c443
YeChan Kim <az20058@naver.com>
2025-03-21 10:05:11 +0900

    🎨 Design : 메인페이지 헤더 UI 개선

--- src/components/home/header/Header.tsx
+++ src/components/home/header/Header.tsx
@@ -1,15 +1,13 @@
 import Image from "next/image";
-import Link from "next/link";
+import NtButton from "./NtButton";
 
 export default function Header() {
   return (
-    <header className="absolute top-0 z-10 flex w-full max-w-[600px] items-center justify-between px-[18px] py-[23px]">
+    <header className="absolute top-0 z-10 flex h-[73px] w-full max-w-[600px] items-center justify-between px-[18px]">
       <h1 className="w-[10%] text-center text-[18px] font-semibold text-[#143967]">홈</h1>
-      <div className="flex w-[20%] items-center justify-center gap-[15px]">
-        <Link className="flex justify-center" href="/notifications">
-          <Image src="/images/bell.svg" width={18} height={19} alt="알림 버튼" />
-        </Link>
-        <Image src="/images/hamburger.svg" width={15} height={13} alt="메뉴 버튼" />
+      <div className="flex w-[20%] items-center gap-[3px]">
+        <NtButton />
+        <Image src="/images/hamburger.svg" width={17} height={17} alt="메뉴 버튼" />
       </div>
     </header>
   );
