a2f0e2fabe0e1364001ed718a4c821880ca12135
YeChan Kim <az20058@naver.com>
2025-03-26 15:54:23 +0900

    🏗️ Change : prefetchQuery->useQuery로 변경 (RCC 패칭)

--- src/components/home/TableSection.tsx
+++ src/components/home/TableSection.tsx
@@ -1,18 +1,10 @@
 import TimeTable from "./TimeTable/TimeTable";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { getQueryClient } from "@/utils/get-query-client";
-import { useTimeTable } from "@/hooks/homeHooks";
 import TableBody from "./TimeTable/TableBody";
 
 export const TableSection = () => {
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(useTimeTable);
-
   return (
     <TimeTable>
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <TableBody />
-      </HydrationBoundary>
+      <TableBody />
     </TimeTable>
   );
 };
