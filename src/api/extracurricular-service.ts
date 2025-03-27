9f891abd089509db26a133ee2fca237f2eeaff48
changhoyu <tyu9817@naver.com>
2025-03-27 18:02:57 +0900

    ♻️ Reafactor : getExtraCurricularPosts 수정

--- src/api/extracurricular-service.ts
+++ src/api/extracurricular-service.ts
@@ -19,9 +19,27 @@ interface ExtraCurricularGetResponse {
   };
 }
 
-export async function getExtraCurricularPosts(): Promise<ExtraCurricularPost[]> {
+export async function getExtraCurricularPosts(
+  page = 0,
+  search_keyword?: string,
+  author: "ALL" | "GYOSU-HAKSEUB" | "JINLO_CHWIEOB" | "DAEHAK_HYEOKSIN" = "ALL",
+  sort_index: "DATE" | "VIEW_COUNT" | "LIKE_COUNT" = "DATE"
+): Promise<ExtraCurricularPost[]> {
+  // search_keyword 없는 경우
+  if (search_keyword === undefined) {
+    const res: AxiosResponse<ExtraCurricularGetResponse> =
+      await privateApi.get<ExtraCurricularGetResponse>(
+        `/ec-notices/sku?page=${page}&author=${author}&sort_index=${sort_index}`
+      );
+
+    return res.data.result.ecNoticeList;
+  }
+
+  // search_keyword 있는 경우
   const res: AxiosResponse<ExtraCurricularGetResponse> =
-    await privateApi.get<ExtraCurricularGetResponse>("/api/notices/sku/extra-curricular");
+    await privateApi.get<ExtraCurricularGetResponse>(
+      `/ec-notices/sku?page=${page}&author=${author}&sort_index=${sort_index}&search_keyword=${search_keyword}`
+    );
 
   return res.data.result.ecNoticeList;
 }
