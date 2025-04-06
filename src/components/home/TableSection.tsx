83f734ae86955f0107ce20c186aa553bb1e30f53
YeChan Kim <az20058@naver.com>
2025-04-06 20:11:47 +0900

    🐛 Fix : 메인페이지 및 마이페이지 데이터 패칭 rcc로 변경

--- src/components/home/TableSection.tsx
+++ src/components/home/TableSection.tsx
@@ -1,18 +1,10 @@
 import TimeTable from "./TimeTable/TimeTable";
 import TableBody from "./TimeTable/TableBody";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { getQueryClient } from "@/utils/get-query-client";
-import { useTimeTable } from "@/hooks/homeHooks";
 
 export const TableSection = () => {
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(useTimeTable);
-
   return (
     <TimeTable>
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <TableBody />
-      </HydrationBoundary>
+      <TableBody />
     </TimeTable>
   );
 };
