b6de3c6447a436261d4955b236ec7e244f10520e
ChangHo Yu <tyu9817@naver.com>
2025-05-04 00:06:41 -0700

    🐛Fix: deploy 오류

--- src/components/notice/button/NoticeKeywordButton.tsx
+++ src/components/notice/button/NoticeKeywordButton.tsx
@@ -1,7 +1,5 @@
 import Image from "next/image";
 
-import trashIcon from "@/assets/images/notice/trashIcon.png";
-
 interface NoticeKeywordBtnProps {
   keyword: string;
 }
@@ -11,7 +9,7 @@ const NoticeKeywordButton = ({ keyword }: NoticeKeywordBtnProps) => {
     <div className="mb-6 flex h-8 w-full items-center justify-between border-b-2 border-[#75869B21] pb-6">
       <p className="ml-1.5 text-[16px] font-medium text-[#143967]">{keyword}</p>
       <button className="mr-1.5 cursor-pointer">
-        <Image src={trashIcon} alt="trash-icon" width={16} height={16} />
+        <Image src="/images/notice/trashIcon.png" alt="trash-icon" width={16} height={16} />
       </button>
     </div>
   );
