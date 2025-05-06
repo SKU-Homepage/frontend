a290489fb8be0f8b6cfff68ffe3f222312912cbe
ChangHo Yu <tyu9817@naver.com>
2025-05-05 17:20:08 -0700

    💡comments: NoticeBtn 주석처리

--- src/app/notice/favorite/page.tsx
+++ src/app/notice/favorite/page.tsx
@@ -11,12 +11,12 @@ export default function FavoriteNotificationPage() {
         title="찜 목록"
         mention="전체 학사 일정과 개인 일정을 추가하여 한눈에 정리할 수 있어요"
       />
-      <NoticeBtn
+      {/* <NoticeBtn
         noticeTitle="test"
         department="test"
         type="favorite"
-        onClick={() => console.log("test")}
-      />
+        // onClick={() => console.log("test")}
+      /> */}
     </>
   );
 }
