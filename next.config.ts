b921317cc3ca28819544867047534c90a5810828
YeChan Kim <az20058@naver.com>
2026-02-15 21:52:58 +0900

    Settings: 터보팩 svg 설정

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ import withPWAInit from "@ducanh2912/next-pwa";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
